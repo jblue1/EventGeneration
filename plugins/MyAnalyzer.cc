94eafbb96a7724b8b7ddc114ec9f641fe60dd0f8
John Blue <john.s.blue98@gmail.com>
2020-10-22 16:45:51 +0200

    added gen and recoCHS jets, bug fixes

--- plugins/MyAnalyzer.cc
+++ plugins/MyAnalyzer.cc
@@ -38,6 +38,10 @@
 #include "DataFormats/JetReco/interface/PFJet.h"
 #include "DataFormats/JetReco/interface/PFJetCollection.h"
 
+#include "DataFormats/JetReco/interface/GenJet.h"
+#include "DataFormats/JetReco/interface/GenJetCollection.h"
+
+
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 
 #include "TTree.h"
@@ -70,11 +74,48 @@ class MyAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
       // ----------member data ---------------------------
       //edm::EDGetTokenT<TrackCollection> tracksToken_;  //used to select what tracks to read from configuration file
       edm::EDGetTokenT<reco::PFJetCollection> pfJetsToken_;
-      edm::EDGetTokenT<std::vector<reco::GenParticle>> genParticleToken_;
+      edm::EDGetTokenT<reco::PFJetCollection> pfJetsCHSToken_;
+      edm::EDGetTokenT<reco::GenJetCollection> genJetsToken_;
+      edm::EDGetTokenT<std::vector<reco::GenParticle>> partonsToken_;
 
-      TTree* recoJetTree;
+      TTree* eventTree;
 
-      std::vector<Float_t> recoJetPt;
+      std::vector<Float_t> pfJetPt;
+      std::vector<Float_t> pfJetEta;
+      std::vector<Float_t> pfJetPhi;
+      std::vector<Float_t> pfJetPx;
+      std::vector<Float_t> pfJetPy;
+      std::vector<Float_t> pfJetPz;
+      std::vector<Float_t> pfJetE;
+
+      std::vector<Float_t> pfJetCHSPt;
+      std::vector<Float_t> pfJetCHSEta;
+      std::vector<Float_t> pfJetCHSPhi;
+      std::vector<Float_t> pfJetCHSPx;
+      std::vector<Float_t> pfJetCHSPy;
+      std::vector<Float_t> pfJetCHSPz;
+      std::vector<Float_t> pfJetCHSE;
+
+      std::vector<Float_t> genJetPt;
+      std::vector<Float_t> genJetEta;
+      std::vector<Float_t> genJetPhi;
+      std::vector<Float_t> genJetPx;
+      std::vector<Float_t> genJetPy;
+      std::vector<Float_t> genJetPz;
+      std::vector<Float_t> genJetE;
+
+      std::vector<Float_t> partonPt;
+      std::vector<Float_t> partonEta;
+      std::vector<Float_t> partonPhi;
+      std::vector<Float_t> partonPx;
+      std::vector<Float_t> partonPy;
+      std::vector<Float_t> partonPz;
+      std::vector<Float_t> partonE;
+      std::vector<Int_t> partonPdgId;
+      std::vector<Int_t> partonStatus;
+
+
+        
 };
 
 //
@@ -91,7 +132,10 @@ class MyAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
 MyAnalyzer::MyAnalyzer(const edm::ParameterSet& iConfig) :
     //tracksToken_(consumes<TrackCollection>(iConfig.getUntrackedParameter<edm::InputTag>("tracks")))
     pfJetsToken_(consumes<reco::PFJetCollection>(iConfig.getParameter<edm::InputTag>("ak4PFJets"))),
-    genParticleToken_(consumes<std::vector<reco::GenParticle>>(iConfig.getParameter<edm::InputTag>("genParticles")))
+    pfJetsCHSToken_(consumes<reco::PFJetCollection>(iConfig.getParameter<edm::InputTag>("ak4PFJets"))),
+    genJetsToken_(consumes<reco::GenJetCollection>(iConfig.getParameter<edm::InputTag>("ak4GenJets"))),
+    partonsToken_(consumes<std::vector<reco::GenParticle>>(iConfig.getParameter<edm::InputTag>("genParticles")))
+
 
 {
    //now do what ever initialization is needed
@@ -115,23 +159,101 @@ MyAnalyzer::~MyAnalyzer()
 // ------------ method called for each event  ------------
 void
 MyAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
-    recoJetPt.clear();
+
+    pfJetPt.clear();
+    pfJetEta.clear();
+    pfJetPhi.clear();
+    pfJetPx.clear();
+    pfJetPy.clear();
+    pfJetPz.clear();
+    pfJetE.clear();
+
+    pfJetCHSPt.clear();
+    pfJetCHSEta.clear();
+    pfJetCHSPhi.clear();
+    pfJetCHSPx.clear();
+    pfJetCHSPy.clear();
+    pfJetCHSPz.clear();
+    pfJetCHSE.clear();
+
+    genJetPt.clear();
+    genJetEta.clear();
+    genJetPhi.clear();
+    genJetPx.clear();
+    genJetPy.clear();
+    genJetPz.clear();
+    genJetE.clear();
+
+    partonPt.clear();
+    partonEta.clear();
+    partonPhi.clear();
+    partonPx.clear();
+    partonPy.clear();
+    partonPz.clear();
+    partonE.clear();
+    partonPdgId.clear();
+    partonStatus.clear();
+
+
     using namespace edm;
     std::cout << "You called analyze" << std::endl;
-    edm::Handle<reco::PFJetCollection> pfjetH;
-    iEvent.getByToken(pfJetsToken_, pfjetH);
+    edm::Handle<reco::PFJetCollection> pfJetH;
+    iEvent.getByToken(pfJetsToken_, pfJetH);
 
-    edm::Handle<std::vector<reco::GenParticle>> genParticles;
-    iEvent.getByToken(genParticleToken_, genParticles);
+    edm::Handle<reco::PFJetCollection> pfJetCHSH;
+    iEvent.getByToken(pfJetsCHSToken_, pfJetCHSH);
+
+    edm::Handle<reco::GenJetCollection> genJetH;
+    iEvent.getByToken(genJetsToken_, genJetH);
+
+    edm::Handle<std::vector<reco::GenParticle>> partonsH;
+    iEvent.getByToken(partonsToken_, partonsH);
    
-    for (reco::PFJetCollection::const_iterator jet=pfjetH->begin(); jet != pfjetH->end(); ++jet) {
-        float pt = jet->pt();
-        std::cout << "Jet PT: " << pt << std::endl;
-        recoJetPt.push_back(pt);
+    for (reco::PFJetCollection::const_iterator jet=pfJetH->begin(); jet != pfJetH->end(); ++jet) {
+        //float pt = jet->pt();
+        //std::cout << "Jet PT: " << pt << std::endl;
+        pfJetPt.push_back(jet->pt());
+        pfJetEta.push_back(jet->eta());
+        pfJetPhi.push_back(jet->phi());
+        pfJetPx.push_back(jet->px());
+        pfJetPy.push_back(jet->py());
+        pfJetPz.push_back(jet->px());
+        pfJetE.push_back(jet->energy());
     }
 
-    for (std::vector<reco::GenParticle>::const_iterator particle_it = genParticles->begin(); particle_it != genParticles->end(); particle_it++) {
-        std::cout << "STATUS: " << particle_it->status() << std::endl;
+    for (reco::PFJetCollection::const_iterator jet=pfJetCHSH->begin(); jet != pfJetCHSH->end(); ++jet) {
+        pfJetCHSPt.push_back(jet->pt());
+        pfJetCHSEta.push_back(jet->eta());
+        pfJetCHSPhi.push_back(jet->phi());
+        pfJetCHSPx.push_back(jet->px());
+        pfJetCHSPy.push_back(jet->py());
+        pfJetCHSPz.push_back(jet->px());
+        pfJetCHSE.push_back(jet->energy());
+    }
+
+    for (reco::GenJetCollection::const_iterator jet=genJetH->begin(); jet != genJetH->end(); ++jet) {
+        genJetPt.push_back(jet->pt());
+        genJetEta.push_back(jet->eta());
+        genJetPhi.push_back(jet->phi());
+        genJetPx.push_back(jet->px());
+        genJetPy.push_back(jet->py());
+        genJetPz.push_back(jet->px());
+        genJetE.push_back(jet->energy());
+    }
+
+    for (std::vector<reco::GenParticle>::const_iterator particle = partonsH->begin(); particle != partonsH->end(); particle++) {
+        if (particle->status() > 69 && particle->status() < 80) {
+            std::cout << "STATUS: " << particle->status() << std::endl;
+            partonPt.push_back(particle->pt());
+            partonEta.push_back(particle->eta());
+            partonPhi.push_back(particle->phi());
+            partonPx.push_back(particle->px());
+            partonPy.push_back(particle->py());
+            partonPz.push_back(particle->pz());
+            partonE.push_back(particle->energy());
+            partonPdgId.push_back(particle->pdgId());
+            partonStatus.push_back(particle->status());
+        }
     }
 
 #ifdef THIS_IS_AN_EVENT_EXAMPLE
@@ -144,7 +266,7 @@ MyAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
    iSetup.get<SetupRecord>().get(pSetup);
 #endif
     
-    recoJetTree->Fill();
+    eventTree->Fill();
 }
 
 
@@ -152,10 +274,41 @@ MyAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
 void
 MyAnalyzer::beginJob() {
     edm::Service<TFileService> fs;
-    recoJetTree = fs->make<TTree>("recoJetTree", "recoJetTree");
-    recoJetTree->Branch("Pt", &recoJetPt);
+    eventTree = fs->make<TTree>("eventTree", "eventTree");
 
-    
+    eventTree->Branch("pfJetPt", &pfJetPt);
+    eventTree->Branch("pfJetEta", &pfJetEta);
+    eventTree->Branch("pfJetPhi", &pfJetPhi);
+    eventTree->Branch("pfJetPx", &pfJetPx);
+    eventTree->Branch("pfJetPy", &pfJetPy);
+    eventTree->Branch("pfJetPz", &pfJetPz);
+    eventTree->Branch("pfJetE", &pfJetE);
+
+    eventTree->Branch("pfJetCHSPt", &pfJetCHSPt);
+    eventTree->Branch("pfJetCHSEta", &pfJetCHSEta);
+    eventTree->Branch("pfJetCHSPhi", &pfJetCHSPhi);
+    eventTree->Branch("pfJetCHSPx", &pfJetCHSPx);
+    eventTree->Branch("pfJetCHSPy", &pfJetCHSPy);
+    eventTree->Branch("pfJetCHSPz", &pfJetCHSPz);
+    eventTree->Branch("pfJetCHSE", &pfJetCHSE);
+
+    eventTree->Branch("genJetPt", &genJetPt);
+    eventTree->Branch("genJetEta", &genJetEta);
+    eventTree->Branch("genJetPhi", &genJetPhi);
+    eventTree->Branch("genJetPx", &genJetPx);
+    eventTree->Branch("genJetPy", &genJetPy);
+    eventTree->Branch("genJetPz", &genJetPz);
+    eventTree->Branch("genJetE", &genJetE);
+
+    eventTree->Branch("partonPt", &partonPt);
+    eventTree->Branch("partonEta", &partonEta);
+    eventTree->Branch("partonPhi", &partonPhi);
+    eventTree->Branch("partonPx", &partonPx);
+    eventTree->Branch("partonPy", &partonPy);
+    eventTree->Branch("partonPz", &partonPz);
+    eventTree->Branch("partonE", &partonE);
+    eventTree->Branch("partonPdgId", &partonPdgId);
+    eventTree->Branch("partonStatus", &partonStatus);
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
--- python/ConfFile_cfg.py
+++ python/ConfFile_cfg.py
@@ -8,11 +8,14 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
                                 # replace 'myfile.root' with the source file you want to use
-                                fileNames = cms.untracked.vstring( 'file:step3_RAW2DIGI_L1Reco_RECO_RECOSIM_EI_PAT_inAODSIM.root')
+                                fileNames = cms.untracked.vstring(
+                                'file:step3_RAW2DIGI_L1Reco_RECO_RECOSIM_EI_PAT_inAODSIM.root')
                             )
 
 process.demo = cms.EDAnalyzer('MyAnalyzer', 
     ak4PFJets=cms.InputTag("ak4PFJets"),
+    ak4PFJetsCHS=cms.InputTag("ak4PFJetsCHS"),
+    ak4GenJets=cms.InputTag("ak4GenJets"),
     genParticles=cms.InputTag("genParticles"))
 
 process.TFileService = cms.Service("TFileService",
