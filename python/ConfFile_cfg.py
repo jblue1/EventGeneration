942664e1e9767588af486375d466cfa98b57ab43
John Blue <john.s.blue98@gmail.com>
2020-10-30 04:23:17 +0100

    handle more file

--- python/ConfFile_cfg.py
+++ python/ConfFile_cfg.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
+#import FWCore.Utilities.FileUtils as FileUtils
 
 process = cms.Process("Demo")
 
@@ -6,13 +7,42 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
+# load filelist
+#fileList = FileUtils.loadListFromFile("filelist.txt")
 process.source = cms.Source("PoolSource",
-                                # replace 'myfile.root' with the source file you want to use
                                 fileNames = cms.untracked.vstring(
-                                'file:TTbar_13TeV_TuneCUETP8M1_2018_RECOSIM_2_inAODSIM.root')
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_1.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_2.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_3.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_4.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_5.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_6.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_7.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_8.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_9.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_10.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_11.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_12.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_13.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_14.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_15.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_16.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_17.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_18.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_19.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_20.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_21.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_22.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_23.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_24.root',
+                                    'file:../data/TTbar_13TeV_TuneCUETP8M1_RECOSIM_25.root',
+                                )
+                                #fileNames = cms.untracked.vstring(
+                                #'file:TTbar_13TeV_TuneCUETP8M1_2018_RECOSIM_2_inAODSIM.root')
                             )
+process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 
-process.demo = cms.EDAnalyzer('MyAnalyzer', 
+process.demo = cms.EDAnalyzer('MyAnalyzer',
     ak4PFJets=cms.InputTag("ak4PFJets"),
     ak4PFJetsCHS=cms.InputTag("ak4PFJetsCHS"),
     ak4GenJets=cms.InputTag("ak4GenJets"),
