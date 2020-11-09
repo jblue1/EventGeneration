dbba4edac9d900433d3e249542a2ab2c14b87dbb
John Blue <john.s.blue98@gmail.com>
2020-11-10 00:52:19 +0100

    use filelist for input files

--- python/ConfFile_cfg.py
+++ python/ConfFile_cfg.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
-#import FWCore.Utilities.FileUtils as FileUtils
+import FWCore.Utilities.FileUtils as FileUtils
 
 process = cms.Process("Demo")
 
@@ -7,40 +7,17 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
+# File service
+process.load("CommonTools.UtilAlgos.TFileService_cfi")
+process.TFileService.fileName=cms.string("out.root")
+
 # load filelist
-#fileList = FileUtils.loadListFromFile("filelist.txt")
-prefix = '../../../genData/ttbar/run7'
+fileList = FileUtils.loadListFromFile("filelist.txt")
 process.source = cms.Source("PoolSource",
-                                fileNames = cms.untracked.vstring(
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_1.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_2.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_3.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_4.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_5.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_6.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_7.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_8.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_9.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_10.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_11.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_12.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_13.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_14.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_15.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_16.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_17.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_18.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_19.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_20.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_21.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_22.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_23.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_24.root',
-                                    'file:' + prefix + '/TTbar_13TeV_TuneCUETP8M1_RECOSIM_25.root',
-                                )
-                                #fileNames = cms.untracked.vstring(
-                                #'file:TTbar_13TeV_TuneCUETP8M1_2018_RECOSIM_2_inAODSIM.root')
-                            )
+    fileNames = cms.untracked.vstring(*fileList)
+)
+
+# since different data gen runs have same event numbers
 process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 
 process.demo = cms.EDAnalyzer('MyAnalyzer',
@@ -49,8 +26,10 @@ process.demo = cms.EDAnalyzer('MyAnalyzer',
     ak4GenJets=cms.InputTag("ak4GenJets"),
     genParticles=cms.InputTag("genParticles"))
 
-process.TFileService = cms.Service("TFileService",
-    fileName = cms.string("out.root")
-    )
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+# how many events to process (-1 = all)
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+
 
 process.p = cms.Path(process.demo)
