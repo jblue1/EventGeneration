add9eec2bbcd37ba29b2779e356447b5239b4902
John Blue <john.s.blue98@gmail.com>
2020-11-11 20:41:56 +0100

    make number of jobs a command line argument

--- genData/example/parallelDriver.sh
+++ genData/example/parallelDriver.sh
@@ -1,7 +1,8 @@
 #Based on commands for standard recipe from runTheMatrix.py: 10824.0 TTbar_13TeV_TuneCUETP8M1_2018_GenSimFull+DigiFull_2018+RecoFull_2018+HARVESTFull_2018+ALCAFull_2018+NanoFull_2018 
 #[1]: 
 touch seeds.txt
-for i in {1..25}
+echo "Running $1 jobs"
+for i in {1..$1}
 do
     cmsRun TTbar_13TeV_TuneCUETP8M1_cfi_GEN_SIM.py \
     outputFile=TTbar_13TeV_TuneCUETP8M1_GEN_SIM_${i}.root &
@@ -10,7 +11,7 @@ wait
 
 
 #[2]: 
-for i in {1..25}
+for i in {1..$1}
 do
     cmsRun step2_DIGI_L1_DIGI2RAW_HLT.py \
     inputFiles=file:TTbar_13TeV_TuneCUETP8M1_GEN_SIM_${i}.root \
@@ -20,7 +21,7 @@ wait
 rm TTbar_13TeV_TuneCUETP8M1_GEN_SIM_*.root
 
 #[3]: 
-for i in {1..25}
+for i in {1..$1}
 do
     cmsRun step3_RAW2DIGI_L1Reco_RECO_RECOSIM_EI_PAT.py \
     inputFiles=file:TTbar_13TeV_TuneCUETP8M1_FEVT_${i}.root \
