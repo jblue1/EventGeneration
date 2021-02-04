d9ba64efaf4ac38e6aec717252370beea8f33790
joblue <joblue@davidson.edu@phy1kuchera.davidson.edu>
2021-02-04 10:53:28 -0500

    Fix bug with loop
    I don't know how bash works so it wasn't actually looping the number
    of times specified by the command line arguement.

--- genData/example/parallelDriver.sh
+++ genData/example/parallelDriver.sh
@@ -2,7 +2,7 @@
 #[1]: 
 touch seeds.txt
 echo "Running $1 jobs"
-for i in {1..$1}
+for ((i=1; i < $1 + 1; i++))
 do
     cmsRun TTbar_13TeV_TuneCUETP8M1_cfi_GEN_SIM.py \
     outputFile=TTbar_13TeV_TuneCUETP8M1_GEN_SIM_${i}.root &
@@ -11,7 +11,7 @@ wait
 
 
 #[2]: 
-for i in {1..$1}
+for ((i=1; i < $1 + 1; i++))
 do
     cmsRun step2_DIGI_L1_DIGI2RAW_HLT.py \
     inputFiles=file:TTbar_13TeV_TuneCUETP8M1_GEN_SIM_${i}.root \
@@ -21,7 +21,7 @@ wait
 rm TTbar_13TeV_TuneCUETP8M1_GEN_SIM_*.root
 
 #[3]: 
-for i in {1..$1}
+for ((i=1; i < $1 + 1; i++))
 do
     cmsRun step3_RAW2DIGI_L1Reco_RECO_RECOSIM_EI_PAT.py \
     inputFiles=file:TTbar_13TeV_TuneCUETP8M1_FEVT_${i}.root \
